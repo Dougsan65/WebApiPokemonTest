426ce3d78388a0f9c6b3447b948d6e6b22025f4e
Douglas <douglasclaudino543@gmail.com>
2024-02-22 00:57:22 -0300

    perfomance improving

--- JS/pokedex.js
+++ JS/pokedex.js
@@ -22,24 +22,35 @@ async function showPokemons() {
     for (let i = start; i < end; i++) {
         const pokemonNome = pokemonsAtuais[i].pokemon_species.name;
 
-
+        // Adicionando a imagem do pokemon
         const imgPoke = document.createElement('li');
         imgPoke.classList.add('pokemon-item');
         imgPoke.innerHTML = `<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png" alt="${pokemonNome}">`;
 
+        // Adicionando o nome do pokemon
+        const numberPokeFormated = (i + 1).toString().padStart(4, '0'); // 0001
         const nomeSpan = document.createElement('span');
         nomeSpan.classList.add('pokemon-name');
         nomeSpan.innerText = pokemonNome;
+        
+        // Adicionando o número do pokemon
+        const numeroPoke = document.createElement('span');
+        numeroPoke.classList.add('pokemon-number');
+        numeroPoke.innerText = numberPokeFormated;
        
+        // Adicionando o número e o nome do pokemon
+        imgPoke.appendChild(numeroPoke);
         imgPoke.appendChild(nomeSpan);
         pokemonDiv.appendChild(imgPoke);
     }
+    
+    // Adicionando o contador de páginas
     const totalPages = Math.ceil(pokemonsAtuais.length / limit);
     document.getElementById('pageCounter').innerText = `Página ${ActualPage} de ${totalPages}`;
 }
 getPokedex();
 
-
+// Eventos dos botões de página
 document.getElementById('previousPage').addEventListener('click', function() {
     ActualPage--;
     if (ActualPage < 1) {
@@ -55,6 +66,7 @@ document.getElementById('nextPage').addEventListener('click', function() {
     showPokemons();
 });
 
+// Eventos das setas do teclado
 document.addEventListener('keydown', function(event) {
     if (event.code === 'ArrowLeft') {
         ActualPage--;
@@ -71,4 +83,17 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
-document.getElementById('getPokemonsPokedex').addEventListener('click', getPokedex);
+document.addEventListener('click', function(event) {
+    const target = event.target;
+    if (target.classList.contains('pokemon-item') || target.parentElement.classList.contains('pokemon-item')) {
+        let pokemonName;
+        if (target.classList.contains('pokemon-item')) {
+            pokemonName = target.querySelector('.pokemon-name').innerText;
+        } else if (target.parentElement.classList.contains('pokemon-item')) {
+            pokemonName = target.parentElement.querySelector('.pokemon-name').innerText;
+        }
+        if (pokemonName) {
+            window.open(`https://www.pokemon.com/br/pokedex/${pokemonName}`, '_blank');
+        }
+    }
+});
